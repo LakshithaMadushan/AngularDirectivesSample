158b29395923d14e742f9afb00ce6a473f5f5c0b
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-09-10 20:37:55 +0530

    add directives commit

--- sampleAppSD/src/app/app.module.ts
+++ sampleAppSD/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgIfComponentComponent } from './ng-if-component/ng-if-component.component';
+import { NgForComponentComponent } from './ng-for-component/ng-for-component.component';
+import { NgSwitchComponentComponent } from './ng-switch-component/ng-switch-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfComponentComponent,
+    NgForComponentComponent,
+    NgSwitchComponentComponent
+
   ],
   imports: [
     BrowserModule
